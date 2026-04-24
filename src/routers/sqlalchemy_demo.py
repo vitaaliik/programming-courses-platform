53880ae89ddca07bd09cadf25aa4b9fa9a88bbe0
vitalik <vitaliklutchak12@gmail.com>
2026-04-24 12:28:45 +0300

    refactor: address SQLAlchemy review comments

--- src/routers/sqlalchemy_demo.py
+++ src/routers/sqlalchemy_demo.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
 
 from src.dependencies.course import get_course_service
@@ -9,6 +11,6 @@ router = APIRouter(prefix="/debug/sqlalchemy", tags=["sqlalchemy-demo"])
 
 @router.get("/courses", response_model=list[CourseRead])
 async def debug_sqlalchemy_courses(
-    course_service: CourseService = Depends(get_course_service),
+    course_service: Annotated[CourseService, Depends(get_course_service)],
 ):
     return course_service.get_all_courses()
